dd609bce207386ee7f75e46783b6ee4c27842160
arthfgp <arthfgp@gmail.com>
2020-09-03 19:46:41 -0300

    Criação cards percentual

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import axios from 'axios';
 import { Box, Grid, Typography, Paper, Container } from '@material-ui/core';
 import ContadorChecksDiario from './ContadorChecksDiario';
 import ListaChecksRecentes from './ListaChecks';
@@ -10,14 +11,31 @@ import Title from '../ui/Title';
 const Dashboard = () => {
     const classes = stylesDashboard();
     const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
+    const [checks, setcheck] = React.useState<any[] | null>(null);
+    React.useEffect(() => {
+        axios.get("/checkmanip/sendChecks").then(response => {
+            setcheck(response.data);
+        });
+    },[])
     return (
         <Container maxWidth="lg" className={classes.container}>
             <Title>Bem vindo ao Smart Maintenance</Title>
             <Grid container spacing={3}>
-                <Grid item xs={12} md={8} lg={9}>
+            <Grid item xs={12} md={4} lg={3}>
                     <Paper className={fixedHeightPaper}>
-                        <Typography>Aqui podemos deixar um gráfico, recados, etc... Ou então remover né</Typography>
+                        {
+                            
+                        }
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4} lg={3}>
+                    <Paper className={fixedHeightPaper}>
+                        semanal
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4} lg={3}>
+                    <Paper className={fixedHeightPaper}>
+                        mensal
                     </Paper>
                 </Grid>
                 <Grid item xs={12} md={4} lg={3}>
