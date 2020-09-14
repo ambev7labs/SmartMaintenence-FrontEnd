2b3c81600c89b0d08ff67a04c1fc3ae7202ea79c
JsTHiago <thiagobaldino5@gmail.com>
2020-09-14 02:49:26 -0300

    adicionada hora no 5W

---
+++ src/components/CincoPorques/DataSection.tsx
@@ -0,0 +1,50 @@
+import React, { FC, Fragment } from "react";
+import { CincoPorquesInfo } from "../../types";
+import DateFnsUtils from "@date-io/date-fns";
+import { Container, TextField } from "@material-ui/core";
+import {
+  KeyboardDateTimePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import stylesCincoPorques from "../../styles/cincoPorques";
+import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
+interface PropsDateSection {
+  dados: CincoPorquesInfo;
+  setDados: React.Dispatch<React.SetStateAction<CincoPorquesInfo>>;
+}
+
+type AllProps = PropsDateSection;
+
+const DataSection: FC<AllProps> = (props: AllProps) => {
+  const classes = stylesCincoPorques();
+  function dataDefaultValue(): string {
+    const date: Date = new Date();
+    const month: number = date.getMonth() + 1;
+    const day: number = date.getDate();
+    const year: number = date.getFullYear();
+
+    return `${year}-${month}-${day}`;
+  }
+  const handleDataChange = (event: MaterialUiPickersDate) => {
+    console.log(event?.toString());
+    props.setDados({
+      ...props.dados,
+      date: event ? new Date(event?.toString()) : new Date(),
+    });
+  };
+  return (
+    <Container className={classes.container}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDateTimePicker
+          value={props.dados.date}
+          format="dd-MM-yyyy HH:mm"
+          label="Data"
+          className={classes.item}
+          helperText="Digite a data e hora da realização deste 5W"
+          onChange={handleDataChange}
+        />
+      </MuiPickersUtilsProvider>
+    </Container>
+  );
+};
+export default DataSection;
--- src/types.ts
+++ src/types.ts
@@ -1,103 +1,103 @@
 export interface GrupoEquipamento {
-    id: number;
-    title: string;
+  id: number;
+  title: string;
 }
 
 export interface Equipamento {
-    date: string;
-    _id: string;
-    name: string;
-    procedures: Procedimento[];
-    field: string;
-    typeEquip: string;
-    __v: number;
-    frequency: number;
-    period: string;
-    link?: string;
+  date: string;
+  _id: string;
+  name: string;
+  procedures: Procedimento[];
+  field: string;
+  typeEquip: string;
+  __v: number;
+  frequency: number;
+  period: string;
+  link?: string;
 }
 export interface Operarios {
-    _id: string;
-    password: string;
-    name: string;
-    userId: string;
-    field: string;
-    admin: boolean;
+  _id: string;
+  password: string;
+  name: string;
+  userId: string;
+  field: string;
+  admin: boolean;
 }
 
 export interface Check {
-    date: string;
-    name: string;
-    procedures: Procedimento[];
-    field: string;
-    typeEquip: string;
-    frequency: number;
-    userId: string;
-    period: string;
-    link?: string;
+  date: string;
+  name: string;
+  procedures: Procedimento[];
+  field: string;
+  typeEquip: string;
+  frequency: number;
+  userId: string;
+  period: string;
+  link?: string;
 }
 
 export interface Procedimento {
-    _id: string;
-    item: string;
-    description: string;
-    method: string;
-    location: string;
-    point: number;
-    condition: 'PARADA' | 'RODANDO';
-    checked: boolean;
+  _id: string;
+  item: string;
+  description: string;
+  method: string;
+  location: string;
+  point: number;
+  condition: "PARADA" | "RODANDO";
+  checked: boolean;
 }
 
 export interface User {
-    userId: string | undefined;
-    admin: boolean;
-    field: string | undefined;
-    name: string | undefined;
-    _id: string | undefined;
+  userId: string | undefined;
+  admin: boolean;
+  field: string | undefined;
+  name: string | undefined;
+  _id: string | undefined;
 }
 
 export interface Relatorio {
-    _id: string;
-    period: string;
-    frequency: number;
-    date: Date;
-    userId: string;
-    field: string;
-    machineName: string;
-    report: string;
-    arrayAllPages: Procedimento[]; // procedimentos
+  _id: string;
+  period: string;
+  frequency: number;
+  date: Date;
+  userId: string;
+  field: string;
+  machineName: string;
+  report: string;
+  arrayAllPages: Procedimento[]; // procedimentos
 }
 
 export interface Porque {
-    number: string;
-    description: string;
+  number: string;
+  description: string;
 }
 
 export interface CincoPorquesInfo {
-    _id: string;
-    use: string;
-    cause: string;
-    comment: string;
-    corrective: string;
-    date: Date;
-    descriptionAnomaly: string;
-    deterioration: string;
-    equip: string;
-    field: string;
-    maintenanceNotes: string;
-    maintenanceOrder: string;
-    shift: string;
-    tag: string;
-    userId: string;
-    whys: Porque[];
+  _id: string;
+  use: string;
+  cause: string;
+  comment: string;
+  corrective: string;
+  date: Date;
+  descriptionAnomaly: string;
+  deterioration: string;
+  equip: string;
+  field: string;
+  maintenanceNotes: string;
+  maintenanceOrder: string;
+  shift: string;
+  tag: string;
+  userId: string;
+  whys: Porque[];
 }
 
 export interface LimpezaInspecaoInfo {
-    period: string;
-    frequency: number;
-    arrayAllPages: Procedimento[][];
-    date: Date;
-    userId: string;
-    field: string;
-    machineName: string;
-    report: string;
+  period: string;
+  frequency: number;
+  arrayAllPages: Procedimento[][];
+  date: Date;
+  userId: string;
+  field: string;
+  machineName: string;
+  report: string;
 }
